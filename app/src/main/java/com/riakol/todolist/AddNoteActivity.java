f9b4f24c7aec484969c145cc12b5b94eb3ade032
Riakol <inspi47@gmail.com>
2026-03-11 15:54:29 +0500

    Add AddNoteActivity and note display to MainActivity
    Define Note data class and note_item.xml layout.\nImplement AddNoteActivity with text input and priority selection.\nUpdate MainActivity with dummy notes list and navigation FAB.\nRegister activity in manifest and add string resources.

---
+++ app/src/main/java/com/riakol/todolist/AddNoteActivity.java
@@ -0,0 +1,73 @@
+package com.riakol.todolist;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class AddNoteActivity extends AppCompatActivity {
+    private EditText editTextNote;
+    private RadioButton radioButtonLow;
+    private RadioButton radioButtonMedium;
+    private Button buttonSave;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_add_note);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        initView();
+
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveNote();
+
+            }
+        });
+    }
+
+    private void saveNote() {
+        String text = editTextNote.getText().toString().trim();
+        int priority = getPriority();
+    }
+
+    private int getPriority() {
+        int priority;
+        if (radioButtonLow.isChecked()) {
+            priority = 0;
+        } else if (radioButtonMedium.isChecked()) {
+            priority = 1;
+        } else {
+            priority = 2;
+        }
+
+        return priority;
+    }
+
+    public void initView() {
+        editTextNote = findViewById(R.id.editTextNote);
+        radioButtonLow = findViewById(R.id.radioButtonLow);
+        radioButtonMedium = findViewById(R.id.radioButtonMedium);
+        buttonSave = findViewById(R.id.buttonSave);
+    }
+
+    public static Intent newIntent(Context context) {
+        return new Intent(context, AddNoteActivity.class);
+    }
+}
--- app/src/main/java/com/riakol/todolist/MainActivity.java
+++ app/src/main/java/com/riakol/todolist/MainActivity.java
@@ -1,14 +1,27 @@
 package com.riakol.todolist;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+    private LinearLayout linearLayotNotes;
+    private FloatingActionButton buttonAddNote;
+    private ArrayList<Note> notes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +33,56 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        initView();
+
+        Random random = new Random();
+
+        for (int i = 0; i < 20; i++) {
+            Note note = new Note(i, "Note " + i, random.nextInt(3));
+            notes.add(note);
+        }
+
+        showNotes();
+        buttonAddNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = AddNoteActivity.newIntent(MainActivity.this);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void showNotes() {
+        for (Note note : notes) {
+            View view = getLayoutInflater().inflate(
+                    R.layout.note_item,
+                    linearLayotNotes,
+                    false);
+            TextView textViewNote = view.findViewById(R.id.textViewNote);
+            textViewNote.setText(note.getText());
+
+            int colorResId;
+            switch (note.getPriority()) {
+                case 0:
+                    colorResId = android.R.color.holo_green_light;
+                    break;
+                case 1:
+                    colorResId = android.R.color.holo_orange_light;
+                    break;
+                default:
+                    colorResId = android.R.color.holo_red_light;
+                    break;
+            }
+
+            int color = ContextCompat.getColor(this, colorResId);
+            textViewNote.setBackgroundColor(color);
+            linearLayotNotes.addView(view);
+        }
+    }
+
+    public void initView() {
+        linearLayotNotes = findViewById(R.id.linearLayotNotes);
+        buttonAddNote = findViewById(R.id.buttonAddNote);
     }
 }
